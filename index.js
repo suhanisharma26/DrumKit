eccc24aea529c54ceb824dee06a95073978b0d79
suhanisharma26 <77916237+suhanisharma26@users.noreply.github.com>
2022-07-03 10:47:53 +0530

    initial files added

---
+++ index.js
@@ -0,0 +1,54 @@
+var btarr = document.querySelectorAll("button");
+var n = btarr.length;
+for (var i = 0; i < n; i++) {
+  btarr[i].addEventListener("click", function() {
+    var buttonclick = this.innerHTML;
+    makesound(buttonclick);
+    buttonAnimation(buttonclick);
+  });
+  }
+  document.addEventListener("keydown",function(event){
+    makesound(event.key);
+    buttonAnimation(event.key);
+  });
+  function makesound(key){
+    switch (key) {
+      case "w":
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break;
+      case "a":
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+        break;
+      case "s":
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+      case "d":
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+      case "j":
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+        break;
+      case "k":
+        var kick = new Audio('sounds/kick-bass.mp3');
+        kick.play();
+        break;
+      case "l":
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+
+      default:console.log(innerHTML);
+    }
+  }
+  function buttonAnimation(anotherkey){
+  var activebutton =   document.querySelector("."+anotherkey);
+  activebutton.classList.add("pressed");
+  setTimeout(function(){
+    activebutton.classList.remove("pressed");
+  },100)
+  }
